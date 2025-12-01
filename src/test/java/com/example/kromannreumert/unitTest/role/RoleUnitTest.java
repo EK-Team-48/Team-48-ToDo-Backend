ec5dca9e956e966fb09260cf481cfb9ddb9995f3
huw02 <hannibal@ussing.com>
2025-12-01 12:33:59 +0100

    næsten færdig med alle integrationstest

--- src/test/java/com/example/kromannreumert/unitTest/role/RoleUnitTest.java
+++ src/test/java/com/example/kromannreumert/unitTest/role/RoleUnitTest.java
@@ -65,7 +65,7 @@ public class RoleUnitTest {
         //ASSERT
         assertNotNull(result);
         assertEquals(4, result.size());
-        assertEquals("ADMIN", result.get(0).role());
+        assertEquals("ADMIN", result.get(0).roleName());
         assertEquals(1L, result.get(0).id());
 
         //VERIFY
@@ -93,7 +93,7 @@ public class RoleUnitTest {
 
         //ASSERT
         assertNotNull(response);
-        assertEquals("ADMIN", response.role());
+        assertEquals("ADMIN", response.roleName());
         assertEquals(1L, response.id());
 
 
